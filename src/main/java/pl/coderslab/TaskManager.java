105795c81215e944d97d53191df743bdb869acb4
Łukasz Zdankowski <lukasz.zdankowski@gmail.com>
2022-12-28 07:50:37 +0100

    tested console colors

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,2 +1,7 @@
-package pl.coderslab;public class TaskManager {
+package pl.coderslab;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColors.GREEN_BOLD + " green bold " + ConsoleColors.RESET + "\n hello");
+    }
 }
